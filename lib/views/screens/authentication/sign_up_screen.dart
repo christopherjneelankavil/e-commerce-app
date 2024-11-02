86d31f0ddb099e705fb08aef99066b163bda4013
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-11-02 10:55:49 +0530

    Started sign up screen

--- lib/views/screens/authentication/sign_up_screen.dart
+++ lib/views/screens/authentication/sign_up_screen.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce_app/views/screens/authentication/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
@@ -6,14 +5,14 @@ import 'package:slide_to_act/slide_to_act.dart';
 
 import '../main/home_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  State<SignUpScreen> createState() => _SignUpScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _SignUpScreenState extends State<SignUpScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool obscureText = true;
   IconData suffixIcon = IconsaxPlusBold.eye_slash;
@@ -43,8 +42,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   SizedBox(height: height * 0.1),
                   Align(
                     alignment: Alignment.topLeft,
-                    child: Text(
-                      'Login',
+                    child: Text('Login',
                       style: GoogleFonts.poppins(
                         fontSize: 30,
                         fontWeight: FontWeight.bold,
@@ -90,8 +88,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     onSubmit: () {
                       if (_formKey.currentState!.validate()) {
-                        Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context){
                           return const HomeScreen();
                         }));
                       } else {
@@ -110,8 +107,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     children: [
                       InkWell(
                         onTap: () {},
-                        child: Text(
-                          'Forgot password?',
+                        child: Text('Forgot password?',
                           style: GoogleFonts.poppins(
                             fontSize: 16,
                             color: const Color(0xFF0550F1),
@@ -121,25 +117,15 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                       Row(
                         children: [
-                          Text(
-                            'New user? ',
+                          Text('New user? ',
                             style: GoogleFonts.poppins(
                               fontSize: 15,
                               color: const Color(0xFF0550F1),
                             ),
                           ),
                           InkWell(
-                            onTap: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return const SignUpScreen();
-                                  },
-                                ),
-                              );
-                            },
-                            child: Text(
-                              'Sign up',
+                            onTap: () {},
+                            child: Text('Sign up',
                               style: GoogleFonts.poppins(
                                 fontSize: 16,
                                 color: const Color(0xFF0550F1),
@@ -181,19 +167,19 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
         suffixIcon: label == 'Password'
             ? IconButton(
-                onPressed: () {
-                  setState(() {
-                    obscureText = !obscureText;
-                    suffixIcon = obscureText
-                        ? IconsaxPlusBold.eye_slash
-                        : IconsaxPlusBold.eye;
-                  });
-                },
-                icon: Icon(
-                  suffixIcon,
-                  color: const Color(0xFF0550F1),
-                ),
-              )
+          onPressed: () {
+            setState(() {
+              obscureText = !obscureText;
+              suffixIcon = obscureText
+                  ? IconsaxPlusBold.eye_slash
+                  : IconsaxPlusBold.eye;
+            });
+          },
+          icon: Icon(
+            suffixIcon,
+            color: const Color(0xFF0550F1),
+          ),
+        )
             : null,
         border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(25)),
