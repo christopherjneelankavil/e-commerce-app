6c2e96889cec7be7b6015a6a34d329f19c5da5fd
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-11-08 12:46:48 +0530

    Added http package and created services/manage_http

---
+++ lib/models/services/manage_http.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+void manageHttpResponse({
+  required http.Response response,
+  required BuildContext context,
+}){
+
+}
