4f05b10826ea4c898bb6f81d844361515cf67c87
christopherjneelankavil <christopherneelankavil@gmail.com>
2024-11-08 13:19:00 +0530

    Updated services/manage_http

--- lib/models/services/manage_http.dart
+++ lib/models/services/manage_http.dart
@@ -1,9 +1,31 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void manageHttpResponse({
   required http.Response response,
   required BuildContext context,
-}){
+  required VoidCallback onSuccess,
+}) {
+  switch (response.statusCode) {
+    case 200: //success
+      onSuccess();
+      break;
+    case 400: //bad request
+      showSnackBar(context, jsonDecode(response.body)['message']);
+      break;
+    case 500: //internal server error
+      showSnackBar(context, jsonDecode(response.body)['error']);
+      break;
+    case 201: //created
+      onSuccess();
+      break;
+    default:
+      showSnackBar(context, 'An error occurred');
+      break;
+  }
+}
 
+void showSnackBar(BuildContext context, String title) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(title)));
 }
